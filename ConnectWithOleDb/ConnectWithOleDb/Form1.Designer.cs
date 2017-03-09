018dd6193a0e186522a420ce1e831fcbe5b853fc
Jiashu Yang <naxx09@gmail.com>
2017-03-09 16:06:20 -0500

    v1.1

--- ConnectWithOleDb/ConnectWithOleDb/Form1.Designer.cs
+++ ConnectWithOleDb/ConnectWithOleDb/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.lbCustomers = new System.Windows.Forms.ListBox();
+            this.dgvStaff = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaff)).BeginInit();
             this.SuspendLayout();
             // 
             // lbCustomers
@@ -36,17 +38,27 @@
             this.lbCustomers.FormattingEnabled = true;
             this.lbCustomers.Location = new System.Drawing.Point(12, 13);
             this.lbCustomers.Name = "lbCustomers";
-            this.lbCustomers.Size = new System.Drawing.Size(260, 238);
+            this.lbCustomers.Size = new System.Drawing.Size(260, 56);
             this.lbCustomers.TabIndex = 0;
+            // 
+            // dgvStaff
+            // 
+            this.dgvStaff.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStaff.Location = new System.Drawing.Point(12, 99);
+            this.dgvStaff.Name = "dgvStaff";
+            this.dgvStaff.Size = new System.Drawing.Size(260, 150);
+            this.dgvStaff.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dgvStaff);
             this.Controls.Add(this.lbCustomers);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStaff)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.ListBox lbCustomers;
+        private System.Windows.Forms.DataGridView dgvStaff;
     }
 }
 
--- ConnectWithOleDb/ConnectWithOleDb/Form1.cs
+++ ConnectWithOleDb/ConnectWithOleDb/Form1.cs
@@ -17,23 +17,33 @@ namespace ConnectWithOleDb
         {
             InitializeComponent();
 
+            // connect with staff database
             string connectionString =
                 "provider=Microsoft.JET.OLEDB.4.0;" +
                 "data source = Database\\staff.mdb";
+
+            // get data from staffTable
             string commandString = "Select FirstName, LastName from staffTable";
 
+            // create a dataset
             OleDbDataAdapter DataAdapter = new OleDbDataAdapter(commandString, connectionString);
 
             DataSet DataSet = new DataSet();
 
+            // fill dataset
             DataAdapter.Fill(DataSet, "staffTable");
 
+            // get a table from dataset
             DataTable DataTable = DataSet.Tables[0];
 
+            // bing dataset with listbox
             foreach(DataRow DataRow in DataTable.Rows)
             {
                 lbCustomers.Items.Add(DataRow["FirstName"] + " " + DataRow["LastName"]);
             }
+
+            // bind dataset with datagridview
+            dgvStaff.DataSource = DataSet.Tables["staffTable"].DefaultView;
         }
     }
 }
