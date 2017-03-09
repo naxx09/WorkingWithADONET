7fb3032fc5cca365cc7f9371b55f5304e4ca07ee
Jiashu Yang <naxx09@gmail.com>
2017-03-09 15:58:46 -0500

    v1

---
+++ ConnectWithOleDb/ConnectWithOleDb/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace ConnectWithOleDb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCustomers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbCustomers
+            // 
+            this.lbCustomers.FormattingEnabled = true;
+            this.lbCustomers.Location = new System.Drawing.Point(12, 13);
+            this.lbCustomers.Name = "lbCustomers";
+            this.lbCustomers.Size = new System.Drawing.Size(260, 238);
+            this.lbCustomers.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbCustomers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbCustomers;
+    }
+}
+
---
+++ ConnectWithOleDb/ConnectWithOleDb/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectWithOleDb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string connectionString =
+                "provider=Microsoft.JET.OLEDB.4.0;" +
+                "data source = Database\\staff.mdb";
+            string commandString = "Select FirstName, LastName from staffTable";
+
+            OleDbDataAdapter DataAdapter = new OleDbDataAdapter(commandString, connectionString);
+
+            DataSet DataSet = new DataSet();
+
+            DataAdapter.Fill(DataSet, "staffTable");
+
+            DataTable DataTable = DataSet.Tables[0];
+
+            foreach(DataRow DataRow in DataTable.Rows)
+            {
+                lbCustomers.Items.Add(DataRow["FirstName"] + " " + DataRow["LastName"]);
+            }
+        }
+    }
+}
