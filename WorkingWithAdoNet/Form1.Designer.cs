16579b2127df173db7a7c3e63e62ca00a6b9a8ed
Jiashu Yang <naxx09@gmail.com>
2017-03-09 12:10:08 -0500

    create

---
+++ WorkingWithAdoNet/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WorkingWithAdoNet
+{
+    partial class AdoNetForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCustomers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbCustomers
+            // 
+            this.lbCustomers.FormattingEnabled = true;
+            this.lbCustomers.Location = new System.Drawing.Point(12, 12);
+            this.lbCustomers.Name = "lbCustomers";
+            this.lbCustomers.Size = new System.Drawing.Size(421, 173);
+            this.lbCustomers.TabIndex = 0;
+            // 
+            // AdoNetForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 207);
+            this.Controls.Add(this.lbCustomers);
+            this.Name = "AdoNetForm1";
+            this.Text = "Ado .Net Form 1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbCustomers;
+    }
+}
+
---
+++ WorkingWithAdoNet/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkingWithAdoNet
+{
+    public partial class AdoNetForm1 : Form
+    {
+        public AdoNetForm1()
+        {
+            InitializeComponent();
+            /*
+            SqlConnection con = new SqlConnection("Server=(localdb)\\ProjectsV13;" +
+                "Trusted_Connection=yes;" +
+                "Integrated Security=True;" +
+                "database=master;");
+            SqlCommand cmd = new SqlCommand("Select * From StaffTable", con);
+            con.Open();
+            SqlDataReader rd = cmd.ExecuteReader();
+            */
+            string connectionString =
+                "Server=(localdb)\\ProjectsV13;" +
+                "Trusted_Connection=yes;" +
+                "Integrated Security=True;" +
+                "database=master;";
+
+            string commandString = "Select FirstName, LastName from StaffTable";
+
+            SqlDataAdapter DataAdapter = new SqlDataAdapter(commandString, connectionString);
+
+            DataSet DataSet = new DataSet();
+
+            DataAdapter.Fill(DataSet, "StaffTable");
+
+            DataTable DataTable = DataSet.Tables[0];
+
+            foreach(DataRow dataRow in DataTable.Rows)
+            {
+                lbCustomers.Items.Add((dataRow)["FirstName"] + " " + dataRow["LastName"]);
+            }
+        }
+    }
+}
