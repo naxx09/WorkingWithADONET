1428791f657fcb98b635dcf461e6ce09a3350d92
Jiashu Yang <naxx09@gmail.com>
2017-03-09 16:16:12 -0500

    v1

---
+++ CustomizedDataSet/CustomizedDataSet/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomizedDataSet
+{
+    public partial class CustomizedDataSet : Form
+    {
+        private SqlConnection myConnection;
+        private DataSet myDataSet;
+        private SqlCommand myCommand;
+        private SqlDataAdapter DataAdapter;
+
+        public CustomizedDataSet()
+        {
+            InitializeComponent();
+            string connectionString =
+                            "Server=(localdb)\\ProjectsV13;" +
+                            "Trusted_Connection=yes;" +
+                            "Integrated Security=True;" +
+                            "database=master;";
+            myConnection = new SqlConnection(connectionString);
+            myConnection.Open();
+
+            // create a DataSet
+            myDataSet = new DataSet();
+            myDataSet.CaseSensitive = true;
+
+            // create a sql command
+            myCommand = new SqlCommand();
+            myCommand.Connection = myConnection;
+            myCommand.CommandText = "Select FirstName, LastName from StaffTable";
+
+            // create a DataAdapter
+            DataAdapter = new SqlDataAdapter();
+            DataAdapter.SelectCommand = myCommand;
+            DataAdapter.TableMappings.Add("Table", "StaffTable");
+
+            // let DataAdapter fill DataSet
+            DataAdapter.Fill(myDataSet);
+
+            // show in DataGridView
+            dataGridView1.DataSource = myDataSet.Tables["StaffTable"].DefaultView;
+        }
+    }
+}
